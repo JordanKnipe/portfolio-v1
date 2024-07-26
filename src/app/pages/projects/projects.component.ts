509c6335f251a98ee782f3e3b7ddfd79835cd512
Jordan Knipe <jordaniknipe@gmail.com>
2024-07-27 09:29:43 +1000

    removed about component and moved it to home page.

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
