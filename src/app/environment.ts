4b35f2aa5c3dbbac456db25183046044382fecf5
JordanKnipe <69141927+JordanKnipe@users.noreply.github.com>
2024-08-06 20:52:38 +1000

    removed keys

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -5,21 +5,3 @@ export const environment = {
 
 
 
-/*
-
-VITE_TODO_API_ITEM=https://yse1hh41i7.execute-api.ap-northeast-1.amazonaws.com/prod/item
-VITE_TODO_API_ITEMS=https://yse1hh41i7.execute-api.ap-northeast-1.amazonaws.com/prod/items
-VITE_TMDB_KEY=f017fd31f53fc47617a83d89912b7ba7
-VITE_TMDB_TV_PROVIDER_URL=https://api.themoviedb.org/3/watch/providers/movie?api_key=f017fd31f53fc47617a83d89912b7ba7&watch_region=au
-VITE_TMDB_TV_LIST_URL=https://api.themoviedb.org/3/search/movie?api_key=f017fd31f53fc47617a83d89912b7ba7&query
-
-VITE_POVIDER_COUNT=0
-
-
-VITE_AWS_SDK_AK=AKIA2YCW2VNKOI6X6QGU
-VITE_AWS_SDK_SAK=4r3rbkP7tRfpdqNhVerJeuLAKgVksem2lrAEqLFb
-
-VITE_AXIOS__TMDB_P1=https://api.themoviedb.org/3/discover/movie?api_key=f017fd31f53fc47617a83d89912b7ba7&page=
-VITE_AXIOS__TMDB_P2=&with_watch_providers=
-
-*/
