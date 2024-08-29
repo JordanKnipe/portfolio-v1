5c0fcb59c6344eedf70c11edfe8c1dcaa5298185
JordanKnipe <69141927+JordanKnipe@users.noreply.github.com>
2024-08-29 18:31:15 +1000

    added naturesclock wip to project list

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -14,6 +14,7 @@ export class CardComponent {
   @Input() description!: string;
   @Input() projectUrl!: string;
   @Input() sourceUrl!: string;
+  @Input() externalSourceUrl!: string;
 
   @Output() onClick = new EventEmitter<void>();
 
